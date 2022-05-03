966de800ceba159bf8daf2b78d845ce851dd67b4
csa <111@1.com>
2022-05-03 23:13:30 +0800

    match-server version:2.0

--- match_system/src/main.cpp
+++ match_system/src/main.cpp
@@ -7,7 +7,10 @@
 #include <thrift/transport/TServerSocket.h>
 #include <thrift/transport/TBufferTransports.h>
 
-#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <queue>
 
 using namespace ::apache::thrift;
 using namespace ::apache::thrift::protocol;
@@ -18,43 +21,97 @@ using namespace  ::match_service;
 
 using namespace std;
 
+struct Task {
+    User user;
+    string type;
+};
+
+struct MessageQueue {
+    queue<Task> q;
+    mutex m;
+    condition_variable cv;
+} message_queue;
+
+class Pool {
+    public:
+        void save_result(int a, int b) {
+            printf("Match Result %d %d\n", a, b);
+        }
+
+        void match() {
+            if (users.size() > 1) {
+                auto a = users[0], b = users[1];
+                users.erase(users.begin());
+                users.erase(users.begin());
+
+                save_result(a.id, b.id);
+            }
+        }
+
+        void add(User user) {
+            users.push_back(user);
+        }
+
+        void remove(User user) {
+            for (uint32_t i = 0; i < users.size(); i++) {
+                if (users[i].id == user.id) {
+                    users.erase(users.begin() + i);
+                    break;
+                }
+            }
+        }
+    private:
+        vector<User> users;
+} pool;
+
 class MatchHandler : virtual public MatchIf {
     public:
         MatchHandler() {
             // Your initialization goes here
         }
 
-        /**
-         * user: 添加的用户信息
-         * info: 附加信息
-         * 在匹配池中添加一个名用户
-         * 
-         * @param user
-         * @param info
-         */
         int32_t add_user(const User& user, const std::string& info) {
             // Your implementation goes here
             printf("add_user\n");
 
+            unique_lock<mutex> lck(message_queue.m);
+            message_queue.q.push({user, "add"});
+            message_queue.cv.notify_all();
+
             return 0;
         }
 
-        /**
-         * user: 删除的用户信息
-         * info: 附加信息
-         * 从匹配池中删除一名用户
-         * 
-         * @param user
-         * @param info
-         */
         int32_t remove_user(const User& user, const std::string& info) {
             // Your implementation goes here
             printf("remove_user\n");
+
+            unique_lock<mutex> lck(message_queue.m);
+            message_queue.q.push({user, "remove"});
+            message_queue.cv.notify_all();
 
             return 0;
         }
 
 };
+
+void consume_task() {
+    while (true) {
+        unique_lock<mutex> lck(message_queue.m);
+        if (message_queue.q.empty()) {
+            message_queue.cv.wait(lck);
+        }
+        else {
+            auto task = message_queue.q.front();
+            message_queue.q.pop();
+            lck.unlock();
+
+            if (task.type == "add") pool.add(task.user);
+            else if (task.type == "remove") pool.remove(task.user);
+
+            pool.match();
+        }
+    }
+}
 
 int main(int argc, char **argv) {
     int port = 9090;
@@ -66,8 +123,9 @@ int main(int argc, char **argv) {
 
     TSimpleServer server(processor, serverTransport, transportFactory, protocolFactory);
 
-    cout << "Start Match Server" << endl;
+    printf("Start Match Server\n");
 
+    thread matching_thread(consume_task);
     server.serve();
     return 0;
 }
